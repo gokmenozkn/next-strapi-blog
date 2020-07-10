982b6469b272572aac6a5d3748ef97d1dcdf95e1
gokmenozkn <kanjey159@gmail.com>
2020-07-10 20:19:21 +0300

    updated meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -7,13 +7,14 @@ export default function Layout({ children, title }) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Coderstwins helps you to become a good web developer. We teach you HTML, CSS and Javascript. Enjoy learning." />
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;500;600;700&display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Domine&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
         <script src="https://kit.fontawesome.com/d0d9868073.js" crossOrigin="anonymous"></script>
         <title> {title} </title>
+        <meta name="google-site-verification" content="FKApUNhSKv1SfKXC0nsKio99Qtsrbrphx8v-DMLdCv4" />
       </Head>
-      {/* <body className="container"></body> */}
       <main className="container">
         {/** Navbar */}
         <section className="section">
